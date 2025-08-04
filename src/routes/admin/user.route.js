52e3b44f70d4f718e84dbd418c9a7b6dd23bf655
umang <190160116045umang@gmail.com>
2025-08-04 17:58:31 +0530

    user api

--- src/routes/admin/user.route.js
+++ src/routes/admin/user.route.js
@@ -1,9 +1,10 @@
 import express from "express";
-import { userList } from "../../controllers/admin/user.controller.js";
+import { me, userList } from "../../controllers/admin/user.controller.js";
 import auth from "../../middlewares/auth.js";
 
 const userRouter = express.Router();
 userRouter.use(auth);
+userRouter.route("/me").get(me);
 userRouter.route("/").get(userList);
 
 export default userRouter;
